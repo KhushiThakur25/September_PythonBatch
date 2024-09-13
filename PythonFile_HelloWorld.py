53db63cd422e36a02bce2bddea56d71d8da7abe6
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-09-13 15:01:43 +0530

    Add files via upload

---
+++ PythonFile_HelloWorld.py
@@ -0,0 +1,25 @@
+print("Python is a programming language..")
+name = "Ram"
+age = 30
+#there is print statement
+print("Name:",name)
+print("Age:",age)
+
+'''import keyword
+keyword.kwlist
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await',
+ 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except',
+ 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda',
+ 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']'''
+
+print("""Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.""")
+
+
