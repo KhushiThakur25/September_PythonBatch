2a86803bec3d779f6d181dd65c0651bc54492e37
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-09-14 12:04:12 +0530

    Add files via upload

---
+++ OpertorsFile.py
@@ -0,0 +1,100 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a = 23
+b = 42
+a + b
+65
+a - b
+-19
+a * b
+966
+a / b
+0.5476190476190477
+a // b
+0
+a ** 2
+529
+a ** b
+1558005952997140033806173725098810522409738596181909282129
+2 ** 10
+1024
+5 % 2
+1
+10 < 2
+False
+10 > 6
+True
+10 <= 5
+False
+10 >= 5
+True
+10 >= 10
+True
+c == 4
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    c == 4
+NameError: name 'c' is not defined
+a == 23
+True
+a != 65
+True
+a + 2
+25
+a
+23
+a = a + 2
+a
+25
+a += 2
+a
+27
+x = 5
+y = 6
+z = 1
+if x < y and x < z:
+    print("X is smallest")
+else:
+    print("X is not smallest")
+
+X is not smallest
+if x < y or x < z:
+    print("X is smallest")
+else:
+    print("X is not smallest")
+
+X is smallest
+f = True
+not f
+False
+st = "Kavita"
+"a" in st
+True
+"z" in st
+False
+>>> x = 5
+>>> y = 5
+>>> x is y
+True
+>>> y = 6
+>>> x is y
+False
+>>> y = x
+>>> x is y
+True
+>>> y is x
+True
+>>> 10 & 5
+0
+>>> 10 & 11
+10
+>>> 10 | 11
+11
+>>> 9 ^ 11
+2
+>>> ~10
+-11
+>>> 10 << 2
+40
+>>> 10 >> 2
+2
