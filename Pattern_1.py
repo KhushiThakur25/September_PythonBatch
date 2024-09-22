e6ca29b15335c51b4d8d3a95e81807d10106f912
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-09-22 11:08:31 +0530

    Add files via upload

---
+++ Pattern_1.py
@@ -0,0 +1,32 @@
+for i in range(5):
+    for j in range(1,6):
+        print(j,end = " ")
+    print()
+
+''' 1 2 3 4 5 
+    1 2 3 4 5 
+    1 2 3 4 5 
+    1 2 3 4 5 
+    1 2 3 4 5 '''
+for i in range(5):
+    for j in range(5,0,-1):
+        print(j,end= " " )
+    print()
+
+'''5 4 3 2 1 
+    5 4 3 2 1 
+    5 4 3 2 1 
+    5 4 3 2 1 
+    5 4 3 2 1 '''
+
+n = 1
+for i in range(4):
+    for j in range(4):
+        print(f"{n} ", end = " ")
+        n += 1
+    print()
+'''1  2  3  4  
+   5  6  7  8  
+   9  10  11  12  
+   13  14  15  16'''
+
